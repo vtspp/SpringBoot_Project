c766d86a2ec06ee1f43605a7c57ac6169eb2f8eb
vtspp <vtspp@gmail.com>
2020-03-26 13:56:48 -0300

    OrderStatus enum

--- src/main/java/com/projectSpring/config/TestConfig.java
+++ src/main/java/com/projectSpring/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.projectSpring.entities.Order;
 import com.projectSpring.entities.User;
+import com.projectSpring.entities.enums.OrderStatus;
 import com.projectSpring.repositories.OrderRepository;
 import com.projectSpring.repositories.UserRepository;
 
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "99999999", "123456");
 		User u2 = new User(null, "Alex Green", "Alex@gmail.com", "99999999", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-06-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-06-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		OrderRepository.saveAll(Arrays.asList(o1, o2, o3));
