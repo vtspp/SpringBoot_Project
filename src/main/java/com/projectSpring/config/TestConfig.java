7cd1861854e57ba7659f163154de18f0e6aecae6
vtspp <vtspp@gmail.com>
2020-03-28 00:09:33 -0300

    Payment, one-to-one association

--- src/main/java/com/projectSpring/config/TestConfig.java
+++ src/main/java/com/projectSpring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projectSpring.entities.Category;
 import com.projectSpring.entities.Order;
 import com.projectSpring.entities.OrderItem;
+import com.projectSpring.entities.Payment;
 import com.projectSpring.entities.Product;
 import com.projectSpring.entities.User;
 import com.projectSpring.entities.enums.OrderStatus;
@@ -54,11 +55,6 @@ public class TestConfig implements CommandLineRunner {
 		Product p4 = new Product(null, "Pc Gamer", "Lorem ipsum dolor sit amet, consectetur.", 1200.00, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Lorem ipsum dolor sit amet, consectetur.", 100.99, "");
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
@@ -66,15 +62,21 @@ public class TestConfig implements CommandLineRunner {
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
 		
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p4.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p1.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
-		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
 		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		orderRepository.saveAll(Arrays.asList(o1));
 	}
 
 }
